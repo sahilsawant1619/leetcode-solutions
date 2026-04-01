d585bf7338765f4ca7faacffc1e52cb0289b00df
Sahil <sahilsawant1619@gmail.com>
2026-04-01 21:33:19 +0530

    Addes 283 MoveZeroes

---
+++ Missing_Number_268.java
@@ -0,0 +1,11 @@
+public class Missing_Number_268 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = n*(n+1)/2;
+
+        for(int num: nums){
+            sum-=num;
+        }
+        return sum;
+    }
+}
